4416b0dab42fc738a236c1e0b1fbbe62a2d70f28
Civodel <tucanbarrera@live.com.mx>
2020-07-18 16:03:22 -0500

    LogicaForm Estable V2

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ inputPresupuesto.addEventListener("mousemove", function actualizarPresupuesto() 
 })
 
 
-
-
 var botonCotizar = document.getElementById("button");
 
 botonCotizar.addEventListener("click", function() {
@@ -15,6 +13,12 @@ botonCotizar.addEventListener("click", function() {
     var valorModelo = document.getElementById("modelOption").value;
     var valorPresupuesto = document.getElementById("presuRange").value;
     var valorTransmision = document.getElementById("transmisionOption").value.toLowerCase();
+
+
+    var padreUser = document.getElementById("padreform");
+    var formUser = document.getElementById("formularioUsuario");
+
+
     if (valorTransmision == "automatico") {
         valorTransmision = "auto";
     }
@@ -22,10 +26,22 @@ botonCotizar.addEventListener("click", function() {
     for (let index = 0; index < autos.length; index++) {
         if (valorMarca == autos[index].make && valorModelo == autos[index].year && valorTransmision == autos[index].transmision && (autos[index].price <= valorPresupuesto * 5)) {
             autosCotizados.push(autos[index]);
+            var div = document.createElement("div");
+            div.id = "carrosOutput";
+            div.className = 'card col-3 shadow-lg p-3 mb-5 bg-white rounded';
+            var autosCardbody = document.createElement('div');
+            autosCardbody.className = 'card-body';
+            var autosMarca = document.createElement('h2')
+            autosMarca.innerHTML = autos[index].make;
+            contenedordeCarros.appendChild(div)
+            div.appendChild(autosCardbody)
+            autosCardbody.appendChild(autosMarca)
+
 
         }
 
     }
+    padreUser.removeChild(formUser);
 
     console.log(autosCotizados);
 })
